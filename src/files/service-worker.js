af6b15405d0fc81a3cda477d0429cce2226a25c1
Bryce Gilhome <bryce.gilhome@camppacific.com>
2019-12-09 19:24:53 -0800

    Change outputDir to outDir to match Gridsome config var name.

--- src/files/service-worker.js
+++ src/files/service-worker.js
@@ -5,21 +5,21 @@ import fs from 'fs-extra'
 const appendToServiceWorker = async (config, options) => {
     const skipWaitingPath = path.resolve(__dirname, '../utils/skip-wating.js')
     const skipWaiting = await fs.readFile(skipWaitingPath, 'utf8')
-    const outPath = path.join(config.outputDir, options.serviceWorkerPath);
+    const outPath = path.join(config.outDir, options.serviceWorkerPath);
     await fs.writeFile(outPath, `\n${skipWaiting}`, { flag: 'a' })
 }
 
 export const createServiceWorker = async (context, config, queue, options) => {
-    const serviceWorkerPath = path.join(config.outputDir, options.serviceWorkerPath)
+    const serviceWorkerPath = path.join(config.outDir, options.serviceWorkerPath)
   
     await generateSW({
       swDest: serviceWorkerPath,
-      globDirectory: config.outputDir,
+      globDirectory: config.outDir,
       globPatterns: [`**\/*.{${options.cachedFileTypes}}`, "**\/*.json"],
       globIgnores: [options.serviceWorkerPath, '**\/*client.json', '**\/*server.json'],
       templatedUrls: queue.reduce((urls, page) => {
         const url = page.path.substring(1)
-        const file = path.relative(config.outputDir, page.htmlOutput)
+        const file = path.relative(config.outDir, page.htmlOutput)
         if (url) urls[url] = file
         return urls
       }, {})
