46e5dc7fe0454d52ba60e180d8c473104e24a0b7
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2020-05-04 16:05:00 +0100

    Add client options

--- src/index.js
+++ src/index.js
@@ -25,7 +25,11 @@ function Plugin (api, options) {
     serviceWorkerPath: path.join(pathPrefix, options.serviceWorkerPath),
     manifestPath: path.join(pathPrefix, options.manifestPath),
     statusBarStyle: options.statusBarStyle,
-    themeColor: options.themeColor
+    themeColor: options.themeColor,
+    icon: options.icon,
+    appleMaskIcon: options.appleMaskIcon,
+    appleMaskIconColor: options.appleMaskIconColor,
+    msTileColor: options.msTileColor,
   })
 }
   
@@ -37,7 +41,7 @@ Plugin.defaultOptions = () => ({
   manifestPath: 'manifest.json',
   disableServiceWorker: true,
   serviceWorkerPath: 'service-worker.js',
-  cachedFileTypes: 'js,json,css,html,png,jpg,jpeg,svg',
+  cachedFileTypes: 'js,json,css,html,png,jpg,jpeg,svg,gif',
   shortName: 'Gridsome',
   themeColor: '#666600',
   backgroundColor: '#ffffff',
